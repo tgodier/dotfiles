621739d4a7351886d5679a428d3d65104aa993d7
tgodier <t.godier@gmail.com>
2020-11-13 15:39:20 -0600

    added install.sh

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Symlink dotfiles
+rm ~/.vimrc
+rm ~/.bashrc
+ln -sv ~/.dotfiles/.vimrc ~
+ln -sv ~/.dotfiles/.bashrc ~
+
+#Reload bash profile
+source ~/.bashrc
+
+# Update Ubuntu and get standard repository programs
+sudo apt update && sudo apt full-upgrade -y
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+# Basics
+install curl
+install git
+install htop
+install vim
+install wget
+
+# Image processing
+#install gimp
+#install jpegoptim
+#install optipng
+
+# Fun stuff
+install figlet
+install lolcat
+install cmus 
+install telegram 
+
+# Get all upgrades
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+# Fun hello
+figlet "Hello!" | lolcat
